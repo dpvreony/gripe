ef859633eeff87221cbeea565320e6501769d8b6
David Vreony <dpvreony@users.noreply.github.com>
2026-01-30 17:07:58 +0000

    add unit test

---
+++ src/Gripe.UnitTests/Analyzer/Analyzers/Winforms/MessageBoxShowShouldBeCalledFromUiElementMemberAnalyzerTests.cs
@@ -0,0 +1,32 @@
+﻿using Gripe.Analyzer;
+using Gripe.UnitTests.Analyzer.Analyzers.EfCore;
+using Microsoft.CodeAnalysis;
+using Gripe.Analyzer.Analyzers.Winforms;
+
+namespace Gripe.UnitTests.Analyzer.Analyzers.Winforms
+{
+    /// <summary>
+    /// Unit test for <see cref="Gripe.Analyzer.Analyzers.Winforms.MessageBoxShowShouldBeCalledFromUiElementMemberAnalyzer"/>.
+    /// </summary>
+    public sealed class MessageBoxShowShouldBeCalledFromUiElementMemberAnalyzerTests : AbstractAnalyzerTest<MessageBoxShowShouldBeCalledFromUiElementMemberAnalyzer>
+    {
+        /// <inheritdoc/>
+        protected override string GetExpectedDiagnosticId()
+        {
+            return DiagnosticIdsHelper.WinformsMessageBoxShowShouldBeCalledFromUiControl;
+        }
+
+        /// <inheritdoc/>
+        protected override ExpectedDiagnosticModel[] GetExpectedDiagnosticLines()
+        {
+            return
+            [
+                new ExpectedDiagnosticModel(
+                    "Winforms\\SimpleForm.cs",
+                    DiagnosticSeverity.Error,
+                    20,
+                    12)
+            ];
+        }
+    }
+}
