4f0cd0893120b3bd50844a7fe80f5b136aee888c
David Vreony <dpvreony@users.noreply.github.com>
2026-01-30 17:18:56 +0000

    add code sample

--- src/Gripe.Testing/Winforms/SimpleForm.cs
+++ src/Gripe.Testing/Winforms/SimpleForm.cs
@@ -8,6 +8,12 @@
         /// <summary>
         /// Calls a message box from a non-member method. Used to ensure GR0052 is NOT triggered.
         /// </summary>
+        /// <example>
+        /// <code>
+        /// var form = new SimpleForm();
+        /// form.CallMessageBoxAsMember();
+        /// </code>
+        /// </example>
         public void CallMessageBoxAsMember()
         {
             System.Windows.Forms.MessageBox.Show("This is a message box NOT called from a UI element member.");
@@ -16,6 +22,11 @@
         /// <summary>
         /// Calls a message box from a non-member method. Used to ensure GR0052 is triggered.
         /// </summary>
+        /// <example>
+        /// <code>
+        /// SimpleForm.CallMessageBoxAsNonMember();
+        /// </code>
+        /// </example>
         public static void CallMessageBoxAsNonMember()
         {
             System.Windows.Forms.MessageBox.Show("This is a message box called from a UI element member.");
