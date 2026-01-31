ec5469bc103ab5846bd6a4b77fa279df543dfb51
David Vreony <dpvreony@users.noreply.github.com>
2026-01-31 22:59:43 +0000

    prep for DoNotUseDirectoryGetFiles

---
+++ src/Gripe.Analyzer/Analyzers/Runtime/DoNotUseDirectoryGetFilesAnalyzer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gripe.Analyzer.Analyzers.Runtime
+{
+    internal class DoNotUseDirectoryGetFilesAnalyzer
+    {
+    }
+}
