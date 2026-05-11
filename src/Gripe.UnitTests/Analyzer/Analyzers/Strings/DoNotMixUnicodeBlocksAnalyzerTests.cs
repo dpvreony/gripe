bac7f81184eb19efe9fd46de17910bc1a6b737f0
David Vreony <dpvreony@users.noreply.github.com>
2026-05-11 16:41:22 +0100

    Feature: check for unicode mixed blocks (#397)

---
+++ src/Gripe.UnitTests/Analyzer/Analyzers/Strings/DoNotMixUnicodeBlocksAnalyzerTests.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2019 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Gripe.Analyzer;
+using Gripe.Analyzer.Analyzers.Strings;
+using Gripe.UnitTests.Analyzer.Analyzers.EfCore;
+using Microsoft.CodeAnalysis;
+
+namespace Gripe.UnitTests.Analyzer.Analyzers.Strings
+{
+    /// <summary>
+    /// Unit Tests for <see cref="DoNotMixUnicodeBlocksAnalyzer"/>.
+    /// </summary>
+    public sealed class DoNotMixUnicodeBlocksAnalyzerTests : AbstractAnalyzerTest<DoNotMixUnicodeBlocksAnalyzer>
+    {
+        /// <inheritdoc/>
+        protected override string GetExpectedDiagnosticId()
+        {
+            return DiagnosticIdsHelper.DoNotMixUnicodeBlocks;
+        }
+
+        /// <inheritdoc/>
+        protected override ExpectedDiagnosticModel[] GetExpectedDiagnosticLines()
+        {
+            const string TupleProofFilePath = "Strings\\UnicodeRangeProof.cs";
+
+            return
+            [
+                new ExpectedDiagnosticModel(
+                    TupleProofFilePath,
+                    DiagnosticSeverity.Warning,
+                    26,
+                    45)
+            ];
+        }
+    }
+}
